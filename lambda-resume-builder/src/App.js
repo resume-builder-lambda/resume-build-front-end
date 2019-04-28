4da58afc717f2729c4a78f67e73505990b6b09bc
Brandon Allison <brandona8128@outlook.com>
2019-04-28 00:00:28 -0400

    began onboarding

--- lambda-resume-builder/src/App.js
+++ lambda-resume-builder/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import LogIn from './Components/LogIn/LogIn'
 import Navigation from './Components/Navigation/Navigation'
 import Dashboard from './Components/Dashboard'
+import Checkout from './Components/OnBoarding/Checkout'
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
       
       <Dashboard/>
+      <Checkout />
        
       </div>
     );
