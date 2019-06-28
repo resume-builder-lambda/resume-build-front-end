34abff1a2fa9e87b3c2165440aef15de0bfbdbfe
Brandon Allison <brandona8128@outlook.com>
2019-06-28 03:12:58 -0400

    WIP: added logout functionality, working on Dashboard router

---
+++ lambda-resume-builder/src/Components/Assignments/Assignments.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+
+const Assignments = () => {
+   
+        return (
+            <h1>This Is The Assignments Checklist Component</h1>
+        )
+    
+        
+    
+    
+    
+}
+
+
+
+
+
+
+
+export default Assignments
---
+++ lambda-resume-builder/src/Components/Calendar/Calendar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+
+const Calendar = () => {
+    
+        return (
+            <h1>This Is The Calendar Checklist Component</h1>
+        )
+    
+        
+    
+    
+    
+}
+
+
+
+
+
+
+
+export default Calendar
