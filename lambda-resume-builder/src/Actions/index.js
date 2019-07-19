c4f688bf97c1a679672cc1be6d218db0cf4cd3b6
brellin <cekakid@gmail.com>
2019-07-19 14:59:30 -0500

    linkedin

--- lambda-resume-builder/src/Actions/index.js
+++ lambda-resume-builder/src/Actions/index.js
@@ -74,12 +74,13 @@ const login = creds => dispatch => {
 const createLinkedInUser = () => {
 
   fetch('https://www.linkedin.com/oauth/v2/accessToken', {
+    method: 'POST',
     headers: {
       grant_type: 'client_credentials',
       client_id: `${process.env.REACT_APP_LINKEDIN_CLIENT_ID}`,
       client_secret: `${process.env.REACT_APP_LINKEDIN_CLIENT_SECRET}`,
       'Access-Control-Allow-Origin': '*',
-      'content-type': 'application/json'
+      'content-type': 'application/x-www-form-urlencoded'
     }
   })
     .then(res => console.log(res))
