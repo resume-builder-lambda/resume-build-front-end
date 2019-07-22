2826a083260876c13599e1012292eb539e1c09c2
brellin <cekakid@gmail.com>
2019-07-22 00:55:16 -0500

    fix gh li 4

--- lambda-resume-builder/src/Actions/index.js
+++ lambda-resume-builder/src/Actions/index.js
@@ -78,7 +78,6 @@ const createLinkedInUser = code => dispatch => {
     window.location = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.REACT_APP_LINKEDIN_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_REGISTER_URI}&scope=r_liteprofile%20r_emailaddress%20w_member_social`
 
     Cookies.set('linkedIn', true)
-    dispatch({ type: LINKEDINRES })
 
   } else {
 
