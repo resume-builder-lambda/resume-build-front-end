c819bbea2abae2622c29ec4059b3b7e049ef8c55
brellin <cekakid@gmail.com>
2019-07-22 00:03:56 -0500

    read the body

--- lambda-resume-builder/src/Actions/index.js
+++ lambda-resume-builder/src/Actions/index.js
@@ -87,7 +87,7 @@ const createLinkedInUser = code => {
         code: code
       }
     })
-      .then(res => console.log(res.getReader()))
+      .then(res => console.log(res.body.getReader()))
       .catch(err => console.log(err))
 
   }
