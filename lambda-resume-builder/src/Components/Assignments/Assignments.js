1cc2b02c2ab766a9605fabcadd3fe7f1193820c3
Brandon Allison <brandona8128@outlook.com>
2019-07-22 13:52:10 -0400

    fixed cards

--- lambda-resume-builder/src/Components/Assignments/Assignments.js
+++ lambda-resume-builder/src/Components/Assignments/Assignments.js
@@ -12,7 +12,7 @@ const Assignments = props => {
             <img alt='' src={props.image} style={{ height: '250PX', width: '98.5%', margin: '.5%', borderRadius: '15px 15px 0 0 ', boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)' }} />
             <hr />
             <h4 ><strong>{props.assignments.name}</strong> </h4>
-            <p>{props.assignments.description}</p>
+           
             </Link>
 
         </div>
