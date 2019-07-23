6715668a7b780c390a4025f2c95c8aac3f3da73c
Brandon Allison <brandona8128@outlook.com>
2019-07-23 04:53:49 -0400

    possible style conflict fix

--- lambda-resume-builder/src/index.js
+++ lambda-resume-builder/src/index.js
@@ -7,15 +7,26 @@ import thunk from 'redux-thunk'
 import rootReducer from './Reducers/rootReducer'
 import logger from 'redux-logger'
 import App from './App'
+import JssProvider from 'react-jss/lib/JssProvider';
+import { createGenerateClassName } from '@material-ui/core/styles';
+
+
 
 import './index.css'
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 
+const generateClassName = createGenerateClassName({
+    dangerouslyUseGlobalCSS: false,
+    productionPrefix: 'c',
+});
+
 ReactDOM.render(
     <Provider store={store}>
         <Router>
+        <JssProvider generateClassName={generateClassName}>
             <App />
+            </JssProvider>
         </Router>
     </Provider>,
     document.getElementById('root')
