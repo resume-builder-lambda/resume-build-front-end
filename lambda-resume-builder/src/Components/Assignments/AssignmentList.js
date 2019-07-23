6c55363be7dcca2f74928d3e8a0d95a056a3e20b
Brandon Allison <brandona8128@outlook.com>
2019-07-22 23:28:52 -0400

    changes to links

--- lambda-resume-builder/src/Components/Assignments/AssignmentList.js
+++ lambda-resume-builder/src/Components/Assignments/AssignmentList.js
@@ -18,19 +18,19 @@ import behavior from '../Images/behavior.jpg'
 const assignments = [
 
     
-    {name: 'Intro to Careers and the Tech Field', img: `${tech}` },
-    {name: 'Financial Planning and Knowing Your Worth', img: `${financial}` },
-    {name: 'Team Dynamics and Feedback: success working in a group',img: `${group}` },
-    {name: 'Social Media in Tech',  img: `${social}` },
-    {name: 'Linkedin',  img: `${linkedin}` },
-    {name: 'Networking Strategies', img: `${networking}` },
-    {name: 'Portfolios and Artifacts', img: `${productivity}` },
-    {name: 'Sharing Your Story', img: `${story}` },
-    {name: 'Intro to Resumes',  img: `${resume}` },
-    {name: 'Behavoiral Interviewing and Star Stories',  img: `${behavior}` },
-    {name: 'Interview Etiquette and Strategies',  img: `${interviewstrat}` },
-    {name: `Offer, Negotiations, NDA's`,  img: `${negotiate}`},
-    {name: 'Job Search Strategy',  img: `${jobsearch}` },
+    {name: 'Intro to Careers and the Tech Field', img: `${tech}`, link:'/dashboard/coming-soon' },
+    {name: 'Financial Planning and Knowing Your Worth', img: `${financial}`, link:'/dashboard/coming-soon' },
+    {name: 'Team Dynamics and Feedback: success working in a group',img: `${group}`, link:'/coming-soon'},
+    {name: 'Social Media in Tech',  img: `${social}`, link:'/dashboard/cold-outreach'},
+    {name: 'Linkedin',  img: `${linkedin}`,link:'/dashboard/cold-outreach' },
+    {name: 'Networking Strategies', img: `${networking}`,link:'/dashboard/cold-outreach' },
+    {name: 'Portfolios and Artifacts', img: `${productivity}`,link:'/dashboard/coming-soon' },
+    {name: 'Sharing Your Story', img: `${story}`,link:'/dashboard/coming-soon' },
+    {name: 'Intro to Resumes',  img: `${resume}`,link:'/dashboard/coming-soon' },
+    {name: 'Behavoiral Interviewing and Star Stories',  img: `${behavior}`,link:'/dashboard/coming-soon' },
+    {name: 'Interview Etiquette and Strategies',  img: `${interviewstrat}`,link:'/dashboard/coming-soon' },
+    {name: `Offer, Negotiations, NDA's`,  img: `${negotiate}`,link:'/dashboard/coming-soon'},
+    {name: 'Job Search Strategy',  img: `${jobsearch}`,link:'/dashboard/coming-soon' },
    
 ]
 
@@ -40,7 +40,7 @@ const AssignmentsList = () => {
         return (
             <div>
                 {assignments.map(assignment => {
-                    return <Assignments assignments={assignment} image={assignment.img} />
+                    return <Assignments assignments={assignment} image={assignment.img} link={assignment.link} />
                 })}
             </div>
         )
---
+++ lambda-resume-builder/src/Components/Assignments/ComingSoon/ComingSoon.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {AssignmentUpload} from '../../AssignmentUpload/AssignmentUpload'
+
+const ComingSoon = () => {
+    
+   
+    return (
+        <div>
+          <p>Training Module Coming Soon...</p>
+        <div style={{marginTop: '50px'}}>
+        <AssignmentUpload/>
+        </div>
+        </div>
+    )
+}
+
+
+
+export default ComingSoon;
