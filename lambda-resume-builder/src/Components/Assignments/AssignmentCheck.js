b72248d4f512d4160a42377fe3de7bf500cc4581
brellin <cekakid@gmail.com>
2019-07-30 16:14:04 -0500

    up to date with brandon's branch

--- lambda-resume-builder/src/Components/Assignments/AssignmentCheck.js
+++ lambda-resume-builder/src/Components/Assignments/AssignmentCheck.js
@@ -1,26 +1,28 @@
 import React from 'react'
 
-const AssignmentCheck = (props) => {
+const AssignmentCheck = props => {
+
 	return (
-		<div
-			style={{
-				color: props.complete ? 'green' : 'red',
-				textDecoration: props.complete ? 'line-through' : 'none',
-				textAlign: 'left',
-				height: 'auto',
-				margin: '10px'
-			}}
-		>
-			<span
-				style={{
-					width: '100%',
-					fontSize: '13px'
-				}}
-			>
-				{props.assignment}
-			</span>
+
+		<div style={{
+			color: props.complete ? 'green' : 'red',
+			textDecoration: props.complete ? 'line-through' : 'none',
+			textAlign: 'left',
+			height: 'auto',
+			margin: '10px'
+		}}>
+
+			<span style={{
+				width: '100%',
+				fontSize: '13px'
+			}}>{props.assignment}</span>
+
+			{props.url && <a href={`${props.url}`}>{props.url}</a>}
+
 		</div>
+
 	)
+
 }
 
 export default AssignmentCheck
