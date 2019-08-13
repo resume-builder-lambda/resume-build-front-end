7c1f097f3e56870b03a67944bb86df40e7fee0e3
blubrandi <blubrandi0@gmail.com>
2019-08-13 10:44:14 -0600

    Created ListItems file for Employment Checklist.

---
+++ lambda-resume-builder/src/Components/EndorsementChecklist/ListItems.js
@@ -0,0 +1,104 @@
+const ListItems = [
+    {
+        text: ' An updated and edited resume (1 page) tailored to your career track. Resume should have: ',
+        isCompleted: false
+    },
+    {
+        text: ' Skills section with all the technologies you have learned listed ',
+        isCompleted: false
+    },
+    {
+        text: 'Projects section featuring the following information:  Project Title with hyperlink to deployed project, One sentence to describe what the project does Tech Stack, Sentence describing deadline, how many people on the team, and your contributions Sentence describing how you used a technology GitHub Repo/ front end / back end link',
+        isCompleted: false
+    },
+    {
+        text: ' “Employment” section with power statements describing transferable skills from previous jobs ',
+        isCompleted: false
+    },
+    {
+        text: ' “Education” section featuring Lambda School and previous schooling ',
+        isCompleted: false
+    },
+    {
+        text: ' Additional sections deemed necessary by coach, such as summary, achievements, activities, etc. ',
+        isCompleted: false
+    },
+    {
+        text: ' Updated LinkedIn profile, including: ',
+        isCompleted: false
+    },
+    {
+        text: ' A clear headshot and cover photo ',
+        isCompleted: false
+    },
+    {
+        text: ' Personal summary to let people get to know you- reference your portfolio site here ',
+        isCompleted: false
+    },
+    {
+        text: ' Work experiences, with descriptions (only include Lambda under work if you were a PM/TL',
+        isCompleted: false
+    },
+    {
+        text: ' Projects, which LI categorizes under “accomplishments” section. Highlight personal, build week, and labs projects, with a link for any deployed projects ',
+        isCompleted: false
+    },
+    {
+        text: ' Education, featuring Lambda School and any previous schooling ',
+        isCompleted: false
+    },
+    {
+        text: ' Green GitHub with quality contributions, including: ',
+        isCompleted: false
+    },
+    {
+        text: ' A clear headshot and professional user handle ',
+        isCompleted: false
+    },
+    {
+        text: ' Top six pinned projects should include any open source contributions and your Build Week or Labs projects ',
+        isCompleted: false
+    },
+    {
+        text: ' Weekly contributions to show ongoing commitment to skills-building ',
+        isCompleted: false
+    },
+    {
+        text: ' Original portfolio website (no templates from drag & drop website builders like wix), including: ',
+        isCompleted: false
+    },
+    {
+        text: ' Your own domain name (no subdomains), purchased through namecheap or other site. (Ask a career coach for a free purchase code to namecheap.com.) ',
+        isCompleted: false
+    },
+    {
+        text: ' Long-form written descriptions of your projects and technical learning/progress ',
+        isCompleted: false
+    },
+    {
+        text: ' Links to LinkedIn, GitHub, Medium, and any other relevant artifacts ',
+        isCompleted: false
+    },
+    {
+        text: ' All Build Weeks and Labs projects with links to code in GitHub ',
+        isCompleted: false
+    },
+    {
+        text: ' OTHER STEPS TO EARN YOUR CAREERS ENDORSEMENT: ',
+        isCompleted: false
+    },
+    {
+        text: ' All careers assignments have been completed and met expectations ',
+        isCompleted: false
+    },
+    {
+        text: ' Passed one SkilledInc. interview and completed three Pramp interviews ',
+        isCompleted: false
+    },
+    {
+        text: ' Social media and other online presence is relevant and professional ',
+        isCompleted: false
+    },
+]
+
+export default ListItems
